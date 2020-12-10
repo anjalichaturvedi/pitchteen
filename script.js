a5cc3af71651876e9e92d5870b1530dfb66136bf
David Tran <tr.davidt@gmail.com>
2020-12-09 19:24:07 -0800

    moved sponsors to slider

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+
+$(document).ready(function(){
+  $('.sponsors').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
